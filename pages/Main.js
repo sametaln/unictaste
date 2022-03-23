ea0106ded12afc7650ea2f87fd6a7cc5551e9974
sametaln <a.sametalan56@gmail.com>
2022-03-24 01:39:10 +0300

    responsive design implemented

--- pages/Main.js
+++ pages/Main.js
@@ -46,9 +46,9 @@ const Main = () => {
                 <ArrowForwardSharpIcon
                   className={styles.arrow}
                   sx={{
-                    fontSize: '4rem',
-                    height: '2rem',
-                    width: '2rem',
+                    fontSize: '2rem',
+                    height: '1rem',
+                    width: '1rem',
                     marginLeft: '0.5rem',
                     color: '#CB4085',
                   }}
@@ -63,62 +63,72 @@ const Main = () => {
       </section>
       <section className={styles.container}>
         <main className={styles.usage} id="usage">
-          <div className={styles.leftImg}>
-            <Image src="/assets/usage.svg" height={404} width={528} />
+          <div className={styles.top}>
+            <h1 className={styles.h1}>Usage</h1>
           </div>
-          <div className={styles.rightText}>
-            <p className={styles.text}>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-              Pellentesque sagittis mauris non ex gravida, sit amet vehicula
-              neque mollis.
-            </p>
-            <div className={styles.linkRoute}>
-              <Link href="/app">
-                <a className={styles.link}>
-                  Go to App
-                  <ArrowRightIcon
-                    className={styles.arrow}
-                    sx={{
-                      fontSize: '4rem',
-                      height: '2rem',
-                      width: '2rem',
-                      color: '#CB4085',
-                    }}
-                  />
-                </a>
-              </Link>
+          <div className={styles.bottom}>
+            <div className={styles.leftImg}>
+              <Image src="/assets/usage.svg" height={404} width={528} />
+            </div>
+            <div className={styles.rightText}>
+              <p className={styles.text}>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                Pellentesque sagittis mauris non ex gravida, sit amet vehicula
+                neque mollis.
+              </p>
+              <div className={styles.linkRoute}>
+                <Link href="/app">
+                  <a className={styles.link}>
+                    Go to App
+                    <ArrowRightIcon
+                      className={styles.arrow}
+                      sx={{
+                        fontSize: '4rem',
+                        height: '2rem',
+                        width: '2rem',
+                        color: '#CB4085',
+                      }}
+                    />
+                  </a>
+                </Link>
+              </div>
             </div>
           </div>
         </main>
       </section>
       <section className={styles.contactContainer}>
         <main className={styles.contact} id="contact">
-          <form className={styles.leftContact}>
-            <input
-              type="text"
-              placeholder="Your Name"
-              className={styles.inputSmall}
-            />
-            <input
-              type="text"
-              placeholder="Your Email Adress"
-              className={styles.inputSmall}
-            />
-            <textarea
-              name="text"
-              placeholder="What is in your mind?"
-              className={styles.inputBig}
-            />
-            <button
-              type="submit"
-              onClick={handleClick}
-              className={styles.submit}
-            >
-              Send
-            </button>
-          </form>
-          <div className={styles.rightContact}>
-            <Image src="/assets/contact2.svg" height={404} width={528} />
+          <div className={styles.top}>
+            <h1 className={styles.contactText}>Contact Us</h1>
+          </div>
+          <div className={styles.bottom}>
+            <form className={styles.leftContact}>
+              <input
+                type="text"
+                placeholder="Your Name"
+                className={styles.inputSmall}
+              />
+              <input
+                type="text"
+                placeholder="Your Email Adress"
+                className={styles.inputSmall}
+              />
+              <textarea
+                name="text"
+                placeholder="What is in your mind?"
+                className={styles.inputBig}
+              />
+              <button
+                type="submit"
+                onClick={handleClick}
+                className={styles.submit}
+              >
+                Send
+              </button>
+            </form>
+            <div className={styles.rightContact}>
+              <Image src="/assets/contact2.svg" height={350} width={450} />
+            </div>
           </div>
         </main>
       </section>
