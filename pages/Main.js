2df6c5ec78bad5925eba47b1e7d16372582c344e
sametaln <a.sametalan56@gmail.com>
2022-03-23 21:01:52 +0300

    front-end completed

---
+++ pages/Main.js
@@ -0,0 +1,124 @@
+import Image from 'next/image';
+import styles from '../styles/Main.module.scss';
+import ArrowForwardSharpIcon from '@mui/icons-material/ArrowForwardSharp';
+import ArrowRightIcon from '@mui/icons-material/ArrowRight';
+import { Button } from '@mui/material';
+
+const Main = () => {
+  const handleClick = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <div>
+      <div className={styles.topbar} id="topbar">
+        <span className={styles.logo}>
+          <a className={styles.logoLink} href="/">
+            Unic<span className={styles.topbarTaste}>taste</span>
+          </a>
+        </span>
+        <nav className={styles.nav}>
+          <a href="#usage" className={styles.linkNav}>
+            Usage
+          </a>
+          <a href="#contact" className={styles.linkNav}>
+            Contact Us
+          </a>
+          <a href="/app" className={styles.linkNav}>
+            App
+          </a>
+        </nav>
+      </div>
+      <section className={styles.container}>
+        <main className={styles.home}>
+          <div className={styles.left}>
+            <p className={styles.text}>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+              Pellentesque sagittis mauris non ex gravida, sit amet vehicula
+              neque mollis.
+            </p>
+            <div className={styles.linkRoute}>
+              <a href="#usage" className={styles.link}>
+                Learn More
+                <ArrowForwardSharpIcon
+                  className={styles.arrow}
+                  sx={{
+                    fontSize: '4rem',
+                    height: '2rem',
+                    width: '2rem',
+                    marginLeft: '0.5rem',
+                    color: '#CB4085',
+                  }}
+                />
+              </a>
+            </div>
+          </div>
+          <div className={styles.right}>
+            <Image src="/assets/main-hero-1.svg" height={404} width={528} />
+          </div>
+        </main>
+      </section>
+      <section className={styles.container}>
+        <main className={styles.usage} id="usage">
+          <div className={styles.leftImg}>
+            <Image src="/assets/usage.svg" height={404} width={528} />
+          </div>
+          <div className={styles.rightText}>
+            <p className={styles.text}>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+              Pellentesque sagittis mauris non ex gravida, sit amet vehicula
+              neque mollis.
+            </p>
+            <div className={styles.linkRoute}>
+              <a href="/" className={styles.link}>
+                Go to App
+                <ArrowRightIcon
+                  className={styles.arrow}
+                  sx={{
+                    fontSize: '4rem',
+                    height: '2rem',
+                    width: '2rem',
+                    color: '#CB4085',
+                  }}
+                />
+              </a>
+            </div>
+          </div>
+        </main>
+      </section>
+      <section className={styles.contactContainer}>
+        <main className={styles.contact} id="contact">
+          <form className={styles.leftContact}>
+            <input
+              type="text"
+              placeholder="Your Name"
+              className={styles.inputSmall}
+            />
+            <input
+              type="text"
+              placeholder="Your Email Adress"
+              className={styles.inputSmall}
+            />
+            <textarea
+              name="text"
+              placeholder="What is in your mind?"
+              className={styles.inputBig}
+            />
+            <button
+              type="submit"
+              onClick={handleClick}
+              className={styles.submit}
+            >
+              Send
+            </button>
+          </form>
+          <div className={styles.rightContact}>
+            <Image src="/assets/contact2.svg" height={404} width={528} />
+          </div>
+        </main>
+      </section>
+    </div>
+  );
+};
+
+export default Main;
--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,11 @@
 import Head from 'next/head';
 import Image from 'next/image';
 import styles from '../styles/Home.module.css';
-import Topbar from '../components/topbar/Topbar';
-import Main from './main-page/Main';
+import Main from './Main';
 
 export default function Home() {
   return (
     <div className={styles.container}>
-      <Topbar />
       <Main />
     </div>
   );
